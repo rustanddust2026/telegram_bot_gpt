a5d5fa1e433e5f0cc24da5ece7e3ba5e03bdc63c
Your Name <rustanddust26@gmail.com>
2026-06-21 11:51:57 +0300

    feat: different fetures

--- bot.py
+++ bot.py
@@ -41,50 +41,66 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     })
 
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_modes[update.message.callback_query.from_user.id] = "RANDOM_MODE"
+    #chat_modes[update.message.from_user.id] = "RANDOM_MODE"
     prompt = load_prompt('random')
     response = await chat_gpt.send_question(prompt,"Give random facts")
     await send_image(update, context, 'random')
-    ##await send_text(update, context, response)
     await send_text_buttons(update,context,response,{
         "random_end":"End",
-        "random_more":"Wanna more facts,"
+        "random_more":"Wanna more facts"
     })
 
 async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_modes[update.message.from_user.id]="GPT_MODE"
+    #response = await chat_gpt.send_question(prompt,"wanna more questions")
     await send_image(update, context, 'gpt')
     await send_text(update, context, load_message('gpt'))
-    # await send_text_buttons(update, context, response, {
+    # await send_text_buttons(update, context, {
     #     "gpt_end": "Stop to Chat",
-
+    #     "gpt_more": "Wanna more facts"
+    #
     # })
 
 async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     mode=chat_modes.get(update.message.from_user.id)
+    #text = update.message.text
     if mode is None:
-        await send_text(update, context, " What a command you used? Press /start command please")
+        pass
+    #     if text == "/start":
+    #         await start(update, context)
+    #     elif text == "/random":
+    #         await random(update, context)
+    #     elif text == "/gpt":
+    #         await gpt(update, context)
+       # await send_text(update, context, " What a command you used? Press /start command please")
     elif mode == "GPT_MODE":
         pt = load_prompt('gpt')
         response = await chat_gpt.send_question(pt,update.message.text)
-             await send_text_buttons(update, context, response, {
-            "gpt_end": "Stop to Chat",
-        })
+        await send_text(update, context, response)
+        chat_modes[update.message.from_user.id] = None
 
 async def gpt_buttons_handler(update: Update, context):
     query = update.callback_query.data
     if query == "gpt_end":
-        chat_modes[update.callback_query.from_user.id] = "None"
+        #chat_modes[update.callback_query.from_user.id] = None
         await start(update, context)
+    elif query == "gpt_more":
+        await gpt(update, context)
+
+    await update.callback_query.answer()
 
 
 
 
-async def buttons_handler(update: Update, context):
+async def random_buttons_handler(update: Update, context):
     query = update.callback_query.data
     if query == "random_end":
         #chat_modes[update.message.from_user.id] = "DEFAULT_MODE"
         await start(update, context)
+    elif query == "random_more":
+        await random(update, context)
+
+    await update.callback_query.answer()
 
 
 
@@ -100,7 +116,7 @@ app.add_handler(CommandHandler('gpt', gpt))
 app.add_handler(MessageHandler(None, text_handler))
 
 #Обробники колбеку:
-app.add_handler(CallbackQueryHandler(buttons_handler, pattern='^random_.*'))
+app.add_handler(CallbackQueryHandler(random_buttons_handler, pattern='^random_.*'))
 app.add_handler(CallbackQueryHandler(gpt_buttons_handler, pattern='^gpt_.*'))
 app.add_handler(CallbackQueryHandler(default_callback_handler))
 
